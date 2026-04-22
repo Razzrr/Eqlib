f77447f4e07efa45145114574c3ff28f8451863c
Razzrr <37455192+Razzrr@users.noreply.github.com>
2026-04-22 09:13:13 -0700

    Swap Trader and Buyer

--- include/eqlib/game/PlayerClient.h
+++ include/eqlib/game/PlayerClient.h
@@ -513,7 +513,7 @@ namespace eqlib
 		/*0x03e8*/ uint8_t FishingEvent;
 		/*0x03ec*/ int Birthdate;
 		/*0x03f0*/ unsigned int LastRangedUsedTime;
-		/*0x03f4*/ int Trader;
+		/*0x03f4*/ int Buyer;
 		/*0x03f8*/ int Unknown0x03f8;
 		/*0x03fc*/ float BearingToTarget;
 		/*0x0400*/ unsigned int LastTick;
@@ -558,7 +558,7 @@ namespace eqlib
 		/*0x0548*/ int64_t HPCurrent;
 		/*0x0550*/ bool bBuffTimersOnHold;
 		/*0x0551*/ char LoginRelated[0x20];
-		/*0x0574*/ int Buyer;
+		/*0x0574*/ int Trader;
 		/*0x0578*/ int ManaMax;
 		/*0x057c*/ unsigned int LoginSerial;
 		/*0x0580*/ uint8_t StandState;
