6d4c365cdce21a214e204eac1fef43a9d36c75dd
Razzrr <37455192+Razzrr@users.noreply.github.com>
2026-08-20 07:27:51 -0700

    Aug-13 patch

--- include/eqlib/game/AltAbilities.h
+++ include/eqlib/game/AltAbilities.h
@@ -35,7 +35,7 @@ struct AltAbilityEffectData
 	int slot;
 };
 
-constexpr size_t CAltAbilityData_size = 0xE0; // @sizeof(CAltAbilityData) :: 2026-06-24 (live) @ 0x14022050D
+constexpr size_t CAltAbilityData_size = 0xE0; // @sizeof(CAltAbilityData) :: 2026-08-13 (live) @ 0x140223B9D
 
 class [[offsetcomments]] CAltAbilityData
 {
